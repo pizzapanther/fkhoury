b16f730138b6deca9c4a8cc7cef98f272f6a15e3
Paul Bailey <paul.m.bailey@gmail.com>
2017-10-21 00:02:32 +0000

    first commit

---
+++ misc/GC/_vti_cnf/MooFlow.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 Apr 2008 05:47:26 -0000
+vti_extenderversion:SR|12.0.0.6211
+vti_author:SR|Fredo-PC\\Fredo
+vti_modifiedby:SR|Fredo-PC\\Fredo
+vti_timecreated:TR|12 Apr 2008 05:47:26 -0000
+vti_cacheddtm:TX|12 Apr 2008 05:47:26 -0000
+vti_filesize:IR|14624
+vti_backlinkinfo:VX|GC/GC_09.html
---
+++ misc/GC/_vti_cnf/mootools.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|27 Feb 2008 14:36:00 -0000
+vti_extenderversion:SR|12.0.0.6211
+vti_author:SR|Fredo-PC\\Fredo
+vti_modifiedby:SR|Fredo-PC\\Fredo
+vti_timecreated:TR|27 Feb 2008 14:36:00 -0000
+vti_cacheddtm:TX|27 Feb 2008 14:36:00 -0000
+vti_filesize:IR|133840
+vti_backlinkinfo:VX|GC/GC_09.html
