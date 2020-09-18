1e92d7ed7c8f82d8583e23ec0e5ceefb45b0f7e4
fkhoury1 <fred@fkhoury.us>
2020-09-17 22:33:56 -0500

    Clean
    Mau

--- Mau/Grace/_vti_cnf/MooFlow.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|12 Apr 2008 05:47:26 -0000
-vti_extenderversion:SR|12.0.0.6211
-vti_author:SR|Fredo-PC\\Fredo
-vti_modifiedby:SR|Fredo-PC\\Fredo
-vti_timecreated:TR|12 Apr 2008 05:47:26 -0000
-vti_cacheddtm:TX|12 Apr 2008 05:47:26 -0000
-vti_filesize:IR|14624
-vti_backlinkinfo:VX|Birthday-6.html Christmas-09_P.html Horses0609-B.html Horses0609-C.html Horses0609-D.html Horses0609-E.html Grace-6.html Horses0609-A.html
--- Mau/Grace/_vti_cnf/mootools.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|27 Feb 2008 14:36:00 -0000
-vti_extenderversion:SR|12.0.0.6211
-vti_author:SR|Fredo-PC\\Fredo
-vti_modifiedby:SR|Fredo-PC\\Fredo
-vti_timecreated:TR|27 Feb 2008 14:36:00 -0000
-vti_cacheddtm:TX|27 Feb 2008 14:36:00 -0000
-vti_filesize:IR|133840
-vti_backlinkinfo:VX|Birthday-6.html Christmas-09_P.html Horses0609-B.html Horses0609-C.html Horses0609-D.html Horses0609-E.html Grace-6.html Horses0609-A.html
