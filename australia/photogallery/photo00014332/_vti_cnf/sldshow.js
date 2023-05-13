811fad30f44c93e5c4ffaf8eb539d1345bf639a5
Paul Bailey <paul@neutron.studio>
2023-05-13 00:59:04 +0000

    reset

---
+++ australia/photogallery/photo00014332/_vti_cnf/sldshow.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|07 Jan 2006 05:54:14 -0000
+vti_author:SR|KHOURY\\fkhoury
+vti_modifiedby:SR|KHOURY\\fkhoury
+vti_nexttolasttimemodified:TW|07 Jan 2006 05:54:14 -0000
+vti_timecreated:TR|12 Mar 2006 22:28:44 -0000
+vti_cacheddtm:TX|12 Mar 2006 22:28:44 -0000
+vti_filesize:IR|4651
+vti_extenderversion:SR|6.0.2.6551
+vti_backlinkinfo:VX|photogallery/photo00014332/real.htm
+vti_syncofs_fkhoury.us\:21/web/australia:TW|12 Mar 2006 22:34:00 -0000
+vti_syncwith_fkhoury.us\:21/web/australia:TX|12 Mar 2006 22:28:44 -0000
