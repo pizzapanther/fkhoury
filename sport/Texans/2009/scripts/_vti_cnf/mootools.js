833a4294d9dc0432bd53f24e85d977f0527ac541
fkhoury1 <fred@fkhoury.us>
2023-05-13 09:08:18 -0500

    Clean

--- sport/Texans/2009/scripts/_vti_cnf/mootools.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|27 Feb 2008 14:36:00 -0000
-vti_extenderversion:SR|12.0.0.6211
-vti_backlinkinfo:VX|2009/Jets.html 2009/Jaguars.html 2009/Saints.html
-vti_author:SR|Fred-PC\\Fred
-vti_modifiedby:SR|Fred-PC\\Fred
-vti_timecreated:TR|27 Feb 2008 14:36:00 -0000
-vti_cacheddtm:TX|27 Feb 2008 14:36:00 -0000
-vti_filesize:IR|133840
