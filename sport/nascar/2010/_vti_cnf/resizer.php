e1e82ca2b251ab27725e93c9bb5fed4677ee124b
Paul Bailey <paul@neutron.studio>
2023-05-13 01:05:16 +0000

    reset

---
+++ sport/nascar/2010/_vti_cnf/resizer.php
@@ -0,0 +1,14 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|14 Jan 2009 14:03:22 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Fred-PC\\Fred
+vti_modifiedby:SR|Fred-PC\\Fred
+vti_timecreated:TR|14 Jan 2009 14:03:22 -0000
+vti_cacheddtm:TX|14 Jan 2009 14:03:22 -0000
+vti_filesize:IR|3461
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|windows-1252
+vti_backlinkinfo:VX|
